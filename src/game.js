49297e409fe25faf2cc6677a20a412960deec7f5
AJ Lobb <ajlobb@live.com>
2022-04-04 15:49:18 +0100

    created some logic

--- src/game.js
+++ src/game.js
@@ -4,17 +4,54 @@ class GameLogic {
     
     playerTurn = () => {
         if (playerX == "B0") {
-            return machine == "B1"
-        } if (playerX == "B3") {
-            return machine == "B6"
-        } if (playerX == "B4") {
+            return machine == "B4"
+        } if (playerX == "B1") {
+            return machine == "B2"
+        } if (playerX == "B6") {
+            return machine == "B5"
+        } if (playerX == "B7") {
             return machine == "B8"
-        } if (playerX == "B5") {
-            return "PlayerX Wins"
         }
-}
-}
 
+        else if (playerX == "B1") {
+            return machine == "B4"
+        } if (playerX == "B0") {
+            return machine == "B2"
+        } if (playerX == "B6") {
+            return machine == "B3"
+        } if (playerX == "B5") {
+            return machine == "B7"
+        } if (playerX == "B8") {
+            return "Draw"
+        }
+        else if (playerX == "B4") {
+            return machine == "B0"
+        } if (playerX == "B1") {
+            return machine == "B7"
+        } if (playerX == "B2") {
+            return machine == "B6"
+        } if (playerX == "B3") {
+            return machine == "B8"
+        } else if (player == "B8"){
+            return machine == "B3"
+        } if (player == "B8") {
+            return machine == "B3"
+        }
+            
+            
+        else if (playerX == "B2") {
+            return machine == "B6"
+        } else if (playerX == "B3") {
+            return machine == "B5"
+        } else if (playerX == "B5") {
+            return machine == "B3"
+        } else if (playerX == "B6") {
+            return machine == "B2"
+        } else if (playerX == "B7") {
+            return machine == "B1"
+        }
+}    }
+}
 module.exports = {
     GameLogic
 }
